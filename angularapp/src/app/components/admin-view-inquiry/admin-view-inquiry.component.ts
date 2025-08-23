0a87096bfab0d81c6246761025e397973e297c34
pvaibhav1002 <pvaibhav2021@gmail.com>
2025-08-23 10:22:41 +0000

    Created all necessary components

---
+++ angularapp/src/app/components/admin-view-inquiry/admin-view-inquiry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-inquiry',
+  templateUrl: './admin-view-inquiry.component.html',
+  styleUrls: ['./admin-view-inquiry.component.css']
+})
+export class AdminViewInquiryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angularapp/src/app/components/admin-view-investment/admin-view-investment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-investment',
+  templateUrl: './admin-view-investment.component.html',
+  styleUrls: ['./admin-view-investment.component.css']
+})
+export class AdminViewInvestmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
