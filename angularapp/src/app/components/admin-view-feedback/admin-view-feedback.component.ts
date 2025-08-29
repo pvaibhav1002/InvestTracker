8d9b8dd8bea257f25145c0a8dd6022b307a98b6c
SaiAsritha-2003 <saiasritha2003@gmail.com>
2025-08-29 12:47:38 +0000

    changes done in admin-view-feedback,user-view-feedback 29-08-25 6:17pm

--- angularapp/src/app/components/admin-view-feedback/admin-view-feedback.component.ts
+++ angularapp/src/app/components/admin-view-feedback/admin-view-feedback.component.ts
@@ -1,47 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedbackService } from 'src/app/services/feedback.service';
- 
+
 @Component({
   selector: 'app-admin-view-feedback',
   templateUrl: './admin-view-feedback.component.html',
   styleUrls: ['./admin-view-feedback.component.css']
 })
 export class AdminViewFeedbackComponent implements OnInit {
- 
+
   feedbacks = [];
+  filterFeedbacks = [];
   selectedUser: any = null;
   selectedInvestment: any = null;
- 
-  constructor(private feedbackService: FeedbackService) {}
- 
+
+
+  constructor(private feedbackService: FeedbackService) { }
+
   ngOnInit(): void {
     this.getFeedbacks();
-    
+
   }
 
   getFeedbacks(): void {
     this.feedbackService.getFeedbacks().subscribe(
       (data) => {
         this.feedbacks = data;
+        this.filterFeedbacks = data;
       },
       (error) => {
         console.error('Error fetching feedback', error);
       }
     );
   }
- 
+
   showUserDetails(user: any): void {
     this.selectedUser = user;
   }
- 
+
   showInvestmentDetails(investment: any): void {
     this.selectedInvestment = investment;
   }
- 
+
   closeModal(): void {
     this.selectedUser = null;
     this.selectedInvestment = null;
   }
+
+  filterByCategory(option: string) {
+    if (option == 'All') {
+      this.filterFeedbacks = this.feedbacks;
+    }
+    else {
+      this.filterFeedbacks = this.feedbacks.filter((feedback) => {
+        feedback.category == option
+      });
+    }
+  }
+
+
+  ascDate: boolean = true;
+
+  sortByDate() {
+    if (this.ascDate)
+      this.feedbacks.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    else
+      this.feedbacks.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    this.ascDate = !this.ascDate;
+  }
+
+  searchByText:string='';
+
+  searchBasedOnText() {
+    this.filterFeedbacks = this.feedbacks;
+    this.filterFeedbacks = this.feedbacks.filter((feed) => {
+      let a = feed.user.username.toLowerCase().includes(this.searchByText.toLowerCase()) || feed.investment.name.toLowerCase().includes(this.searchByText.toLowerCase()) ||
+        feed.category.toLowerCase().includes(this.searchByText.toLowerCase())||feed.feedbackText.toLowerCase().includes(this.searchByText.toLowerCase());
+      return a;
+    });
+  }
+
+
+
+
+
+
 }
- 
- 
+
