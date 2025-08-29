7a5c285b4a7ec9c56396d6d486f4a1e9122a78d0
preethipanguluri <panguluripreethi@gmail.com>
2025-08-29 12:45:31 +0000

    Done changes in user-addInquiry and user-viewInquiry components in frontend 29-08-25 6:15PM

--- angularapp/src/app/components/user-view-inquiry/user-view-inquiry.component.ts
+++ angularapp/src/app/components/user-view-inquiry/user-view-inquiry.component.ts
@@ -15,6 +15,12 @@ export class UserViewInquiryComponent implements OnInit {
   invests: InvestmentInquiry[] = [];
   userId: number;
 
+  filterStatus: string = '';
+  filterTolerance: string = '';
+  searchText: string = '';
+  originalInvests: InvestmentInquiry[] = [];
+
+
   constructor(private investInquiryService: InvestmentInquiryService, private authService: AuthService) { }
 
   ngOnInit(): void {
@@ -23,6 +29,24 @@ export class UserViewInquiryComponent implements OnInit {
       this.invests = data;
     });
 
+  }
+
+  filterText() {
+    this.invests = this.invests.filter((inquiry) => {
+      let a = inquiry.investment.name.toLowerCase().includes(this.searchText.toLowerCase());
+      let b = inquiry.questions.toLowerCase().includes(this.searchText.toLowerCase());
+      let c = inquiry.adminResponse.toLowerCase().includes(this.searchText.toLowerCase());
+      return a || b || c;
+    })
+  }
+
+
+  applyFilters(): void {
+    this.invests = this.originalInvests.filter(inquiry => {
+      let statusMatch = this.filterStatus === '' || inquiry.status === this.filterStatus;
+      let toleranceMatch = this.filterTolerance === '' || inquiry.priority === this.filterTolerance;
+      return statusMatch && toleranceMatch;
+    });
   }
 
 }
