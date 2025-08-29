e0aa5b062dfd5ae52d9dcee8de6c28025b492c5e
Deepu1412 <deepikamuppalla1@gmail.com>
2025-08-29 03:58:24 +0000

    user-add-feedback component changed,admin-edit-component changed 29-08-25 9:28 am

--- angularapp/src/app/components/admin-edit-investment/admin-edit-investment.component.ts
+++ angularapp/src/app/components/admin-edit-investment/admin-edit-investment.component.ts
@@ -2,28 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Investment } from 'src/app/models/investment.model';
-
-
+ 
+ 
 import { InvestmentService } from 'src/app/services/investment.service';
-
+ 
 @Component({
   selector: 'app-admin-edit-investment',
   templateUrl: './admin-edit-investment.component.html',
   styleUrls: ['./admin-edit-investment.component.css']
 })
 export class AdminEditInvestmentComponent implements OnInit {
-  investment:Investment;
+  investment: Investment;
   investmentId: number;
   investmentForm: FormGroup;
-
-  constructor(
-    private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private investmentService: InvestmentService, private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.investmentId = this.activatedRoute.snapshot.params['id'];
-
-    this.investmentForm = this.formBuilder.group({
+  updated: boolean = false;
+  constructor(private fb: FormBuilder, private ar: ActivatedRoute, private is: InvestmentService, private router: Router) {
+    this.investmentForm = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       type: ['', Validators.required],
@@ -33,26 +27,31 @@ export class AdminEditInvestmentComponent implements OnInit {
       purchaseDate: ['', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]],
       status: ['', Validators.required]
     });
-
-    this.getInvestment();
   }
-
-  getInvestment() {
-    this.investmentService.getInvestmentById(this.investmentId).subscribe((data) => {
-      this.investment=data;
+ 
+  ngOnInit(): void {
+    this.investmentId = this.ar.snapshot.params['id'];
+    this.is.getInvestmentById(this.investmentId).subscribe((data) => {
+      this.investment = data;
       this.investmentForm.patchValue(data);
     });
   }
-
+ 
   updateInvestment() {
     if (this.investmentForm.valid) {
-      this.investmentService.updateInvestment(this.investmentId, this.investmentForm.value).subscribe(() => {
-        alert('Investment updated successfully!');
-        this.router.navigate(['/admin-view-investment']);
+      this.is.updateInvestment(this.investmentId, this.investmentForm.value).subscribe(() => {
+        console.log('Investment updated successfully!');
+        this.updated = true;
       });
     }
   }
-
+ 
+  closeModal() {
+    this.updated = false;
+    this.router.navigate(['/admin-view-investment']);
+ 
+  }
+ 
   get f() {
     return this.investmentForm.controls;
   }
