9dd72af97eae6d88e7ab8883adc124ec874eebf1
SaiAsritha-2003 <saiasritha2003@gmail.com>
2025-09-03 04:37:03 +0000

    changes made admin-console,admin-view-inquiry,user-portfolio 03-09-25 9:52am

--- angularapp/src/app/components/admin-add-investment/admin-add-investment.component.ts
+++ angularapp/src/app/components/admin-add-investment/admin-add-investment.component.ts
@@ -1,37 +1,26 @@
 import { Component } from '@angular/core';
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 import { Router } from '@angular/router';
-
 import { InvestmentService } from 'src/app/services/investment.service';
- 
+
 @Component({
 
   selector: 'app-admin-add-investment',
-
   templateUrl: './admin-add-investment.component.html',
-
   styleUrls: ['./admin-add-investment.component.css']
 
 })
 
 export class AdminAddInvestmentComponent {
-
   investmentForm: FormGroup;
-
   addedInvestment: boolean = false;
-
   investmentData: any;
-
   date: Date = new Date();
- 
+
   constructor(
 
     private readonly investmentService: InvestmentService,
-
     private readonly fb: FormBuilder,
-
     private readonly router: Router
 
   ) {
@@ -39,70 +28,47 @@ export class AdminAddInvestmentComponent {
     this.investmentForm = fb.group({
 
       name: ['', [Validators.required, Validators.minLength(5)]],
-
       description: ['', [Validators.required, Validators.minLength(15)]],
-
       type: ['', Validators.required],
-
       price: ['', [Validators.required, Validators.min(0)]],
-
       quantity: ['', [Validators.required, Validators.min(1)]],
-
       capSize: ['', Validators.required],
-
       sector: ['', Validators.required]
 
     });
 
   }
- 
+
   addNewInvestment(form: FormGroup) {
 
     this.investmentData = {
-
       name: form.value.name,
-
       description: form.value.description,
-
       type: form.value.type,
-
       price: form.value.price,
-
       quantity: form.value.quantity,
-
-      postedDate: this.date.toISOString().split('T')[0], // ✅ matches LocalDate (yyyy-MM-dd)
-
+      postedDate: this.date.toISOString().split('T')[0],
       capSize: form.value.capSize,
-
       sector: form.value.sector,
-
       status: 'Active'
 
     };
- 
+
     this.investmentService.addInvestment(this.investmentData).subscribe(() => {
-
       this.addedInvestment = true;
-
-      // console.log("✅ Added investment:", this.investmentData);
 
     });
 
   }
- 
+
   closeAddSuccessPage() {
-
     this.addedInvestment = false;
-
     this.router.navigate(['/admin-view-investment']);
 
   }
- 
   get f() {
-
     return this.investmentForm.controls;
 
   }
 
 }
- 
--- angularapp/src/app/components/admin-console/admin-console.component.ts
+++ angularapp/src/app/components/admin-console/admin-console.component.ts
@@ -1,66 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ChartType } from 'chart.js';
-
 import { Label } from 'ng2-charts';
-
 import { ChartService } from 'src/app/services/chart.service';
  
 @Component({
- 
   selector: 'app-admin-console',
- 
   templateUrl: './admin-console.component.html',
- 
   styleUrls: ['./admin-console.component.css']
- 
 })
  
 export class AdminConsoleComponent implements OnInit {
-
   distribution: any;
-
   constructor(private readonly chartDto: ChartService) { }
-
   TypeLabels: Label[] = [];
-
   TypeData: number[] = [];
-
   SectorLabels: Label[] = [];
-
   SectorData: number[] = [];
-
   CapSizeLabels: Label[] = [];
-
   CapSizeData: number[] = [];
-
   totalUsers: number = 0;
-
   totalInvestments: number = 0;
-
   totalInvestedAmount: number = 0;
-
   userSummaries: any[] = [];
-
   doughnutChartType: ChartType = 'doughnut';
- 
   doughnutChartColors: Array<any> = [
-
     {
-
       backgroundColor: [
-
-        '#007bff', // Blue
-
-        '#28a745', // Green
-
-        '#17a2b8', // Teal
-
-        '#ffc107', // Yellow
-
-        '#dc3545', // Red
-
-        '#6f42c1'  // Purple
+        '#007bff', 
+        '#28a745', 
+        '#17a2b8', 
+        '#ffc107', 
+        '#dc3545', 
+        '#6f42c1'  
 
       ]
 
@@ -71,27 +42,16 @@ export class AdminConsoleComponent implements OnInit {
   ngOnInit() {
 
     this.chartDto.getAdminConsoleData().subscribe(data => {
-
       this.distribution = data;
-
       this.totalUsers = data.totalUsers;
-
       this.totalInvestments = data.totalInvestments;
-
       this.totalInvestedAmount = data.totalInvestedAmount;
-
       this.TypeLabels = Object.keys(this.distribution.distributionByType);
-
       this.TypeData = Object.values(this.distribution.distributionByType);
-
       this.SectorLabels = Object.keys(this.distribution.distributionBySector);
-
       this.SectorData = Object.values(this.distribution.distributionBySector);
-
       this.CapSizeLabels = Object.keys(this.distribution.distributionByCapSize);
-
       this.CapSizeData = Object.values(this.distribution.distributionByCapSize);
-
       this.userSummaries = data.userSummaries;
 
     });
